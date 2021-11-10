d70c353d81f99ecde777cc19517d83a66caa28ae
Ana Pimentel <ana.pimentel@north27.co.uk>
2021-11-10 14:38:06 +0000

    updated function_exercises_01.py

--- function_exercises_01.py
+++ function_exercises_01.py
@@ -1,14 +1,8 @@
-# Function Tasks
+# Function Exercises
 #
 #
-# Let's see if you can solve these word problems by creating functions.
-# The function "skeleton" has been set up for you to fill in below the problem
-# description, as well as example outputs of what the function should return
-# given certain inputs. Best of luck, some of these will be challenging!
-#
-# The tasks will start off easy and proceed to get harder and harder.
 
-# ## Task 1
+# ## Exercise 1
 #
 #  Create a function that takes in two integers and returns
 ## a Boolean True if their sum is 10, False if their sum is something else.
@@ -17,9 +11,7 @@ def check_ten(n1,n2):
     return (n1 + n2) == 10
     
 
-
-
-# ## Task 2
+# ## Exercise 2
 #
 # Create a function that takes in two integers and returns True if their
 # sum is 10, otherwise, return the actual sum value.
@@ -32,7 +24,7 @@ def check_ten_sum(n1,n2):
 
 
 
-# ## Task 3
+# ## Exercise 3
 #
 # Create a function that takes in a string and returns back the
 # first character of that string in upper case.
@@ -44,14 +36,10 @@ def first_upper(mystring):
 
 
 
-# ## Task 4
+# ## Exercise 4
 #
 # Create a function that takes in a string and returns the last two characters.
 # If there are less than two chracters, return the string:  "Error".
-# Use this link if you need help/hint.
-# (https://stackoverflow.com/questions/7983820/get-the-last-4-characters-of-a-string)
-
-
 
 def last_two(mystring):
     if len(mystring) < 2:
@@ -60,10 +48,10 @@ def last_two(mystring):
         return mystring[-2:]
 
 
-# ## Task 5
+# ## Exercise 5
 #
 # Given a list of integers, return True if the sequence [1,2,3] is somewhere
-# in the list. Hint: Use slicing and a for loop.
+# in the list.
 
 
 #fix it:
@@ -86,11 +74,11 @@ def seq_check(nums):
     
 
 
-# ## Task 6
+# ## Exercise 6
 #
 # Given a 2 strings, create a function that returns the difference in length
 # between them. This difference in length should always be a positive number
-# (or just 0). Hint: Absolute Value.**
+# (or just 0). 
 
 
 
@@ -99,7 +87,7 @@ def compare_len(s1,s2):
 
 
 
-# ## Task 7
+# ## Exercise 7
 #
 # Given a list of integers, if the length of the list is an even number,
 # return the sum of the list. If the length of the list is odd, return the max
